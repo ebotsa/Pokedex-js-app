1cfb2bf8a0a46bc33c1b35521aa67005217c236f
ebotsa <87139016+ebotsa@users.noreply.github.com>
2021-08-04 20:05:27 +0200

    Introduced a variable called pokemonList with its objects having ‘name’, ‘type’ and ‘height’ values assigned
    Introduced a variable called pokemonList with its objects having ‘name’, ‘type’ and ‘height’ values assigned

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello World');
-let favoriteFood = 'Fried Rice';
-document.write(favoriteFood);
+let pokemonList = [{}, {}] 
+		let pokemonList = [
+		  { pokename: 'Bulbasaur', height: 7, types: ['Grass', 'Poison'] },
+		  { pokename: 'Charizard', height: 1.7, types: ['Fire', 'Flying'] },
+		  { pokename: 'Butterfree', height: 1.1, types: ['Bug', 'Flying'] },
+          { pokename: 'Pidgeot', height: 1.5, types: ['Flying', 'Normal'] },
+          { pokename: 'Beedrill', height: 1, types: ['Bug', 'Poison'] }]

3dcbb7bf91223af61b31700e992c54e6139327a4
ebotsa <87139016+ebotsa@users.noreply.github.com>
2021-08-04 21:58:45 +0200

    Modified pokemonList structure
    Modified pokemonList structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-let pokemonList = [{}, {}] 
-		let pokemonList = [
-		  { pokename: 'Bulbasaur', height: 7, types: ['Grass', 'Poison'] },
-		  { pokename: 'Charizard', height: 1.7, types: ['Fire', 'Flying'] },
-		  { pokename: 'Butterfree', height: 1.1, types: ['Bug', 'Flying'] },
-          { pokename: 'Pidgeot', height: 1.5, types: ['Flying', 'Normal'] },
-          { pokename: 'Beedrill', height: 1, types: ['Bug', 'Poison'] }]
+let pokemonList = [{
+	pokename: 'Bulbasaur',
+	height: 7,
+	types: ['Grass', 'Poison']
+}, {
+	pokename: 'Charizard',
+	height: 1.7,
+	types: ['Fire', 'Flying']
+}, {
+	pokename: 'Butterfree',
+	height: 1.1,
+	types: ['Bug', 'Flying']
+}, {
+	pokename: 'Pidgeot',
+	height: 1.5,
+	types: ['Flying', 'Normal']
+}, {
+	pokename: 'Beedrill',
+	height: 1,
+	types: ['Bug', 'Poison']
+}]

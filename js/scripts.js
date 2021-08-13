5b55dd1f9b893666f40b6a84166a90640d17b486
ebotsa <87139016+ebotsa@users.noreply.github.com>
2021-08-13 22:14:05 +0200

    Introducing add & getAll IIFE; part two of Exercise 1.5
    Introducing add & getAll IIFE; part two of Exercise 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,36 @@
-// objects in pokemonList
-let pokemonList = [{
-	pokename: 'Bulbasaur',
-	height: 0.7,
-	types: ['Grass', 'Poison']
-}, {
-	pokename: 'Charizard',
-	height: 1.7,
-	types: ['Fire', 'Flying']
-}, {
-	pokename: 'Butterfree',
-	height: 1.1,
-	types: ['Bug', 'Flying']
-}, {
-	pokename: 'Pidgeot',
-	height: 1.5,
-	types: ['Flying', 'Normal']
-}, {
-	pokename: 'Beedrill',
-	height: 1,
-	types: ['Bug', 'Poison']
-}];
-//puts an array of objects in pokemonList
-// forEach list Pokemon names height and types
-pokemonList.forEach(function(pokemon) {
-	console.log(pokemon.pokename + '  ' + pokemon.height + ' ' + pokemon.types + ' ');
-});
+let pokemonRepository = (function() {
+	let pokemonList = [{
+		pokename: 'Bulbasaur',
+		height: 7,
+		types: ['Grass', 'Poison']
+	}, {
+		pokename: 'Charizard',
+		height: 1.7,
+		types: ['Fire', 'Flying']
+	}, {
+		pokename: 'Butterfree',
+		height: 1.1,
+		types: ['Bug', 'Flying']
+	}, {
+		pokename: 'Pidgeot',
+		height: 1.5,
+		types: ['Flying', 'Normal']
+	}, {
+		pokename: 'Beedrill',
+		height: 1,
+		types: ['Bug', 'Poison']
+	}];
 
+	function add(pokemon) {
+		pokemonList.push(pokemon);
+	}
+
+	function getAll() {
+		return pokemonList;
+	}
+	return {
+		add: add,
+		getAll: getAll
+	};
+})();
+console.log(pokemonRepository.getAll());
